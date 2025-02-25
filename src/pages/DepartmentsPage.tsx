f99f06d004423c38fb5d26832c055aadc5f798e3
Govardhana subhash meda <150647401+Govardhan-subhash@users.noreply.github.com>
2025-02-25 14:12:45 +0530

    "subhash1"

--- src/pages/DepartmentsPage.tsx
+++ src/pages/DepartmentsPage.tsx
@@ -17,8 +17,11 @@ function DepartmentsPage() {
     { name: 'Electronics and Communication Engineering', description: 'Learn about electronics and communication.', image: 'https://as2.ftcdn.net/v2/jpg/05/48/65/99/1000_F_548659945_i9QSx59orL39GlYn5CCEWknPogLOXiZG.jpg', path: '/Departments/CSE.tsx' },
     { name: 'Electrical and Electronics Engineering', description: 'Learn about electrical engineering.', image: 'https://krce.ac.in/blog/wp-content/uploads/2024/04/7fb5b63c34cf82142319b728d58716d0.jpg', path: '/departments/eee' },
     { name: 'Mechanical Engineering', description: 'Learn about mechanical engineering.', image: 'https://i.pinimg.com/736x/00/f6/02/00f602fe67d3b51d2a1316f9650ed862.jpg', path: '/departments/mech' },
+
+    { name: 'Master of Business Administration', description: 'Learn about business administration.', image: 'https://i.pinimg.com/736x/ec/b2/6f/ecb26f7c3f6b7956bec90495c15c1a87.jpg', path: '/departments/mba' },
+    {name:'Pharmaceutical science',description:'Learn about Pharmaceutical science.' ,image:'https://as2.ftcdn.net/jpg/02/76/69/43/1000_F_276694328_7eeiKPIDOGR4LaSYhKBGjEmC7ERexPxv.webp',path:'/department/pharm'},
     { name: 'Master of Business Administration', description: 'Learn about business administration.', image: 'https://i.pinimg.com/736x/ec/b2/6f/ecb26f7c3f6b7956bec90495c15c1a87.jpg', path: '.' },
-    
+
   ];
 
   const handleRegisterClick = (path: string) => {
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/TechEuphoria2k25",
   base: "/",  // Adjust if needed
   server: {
     host: "0.0.0.0",  // Allow external access
