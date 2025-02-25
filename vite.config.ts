0837a48ef66c4e4ba4759ad7b267ddedc2e83ee6
Vinay Siddha <121566763+VinaySiddha@users.noreply.github.com>
2025-02-25 14:31:24 +0530

    Updatevite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
 
-  base: "/TechEuphoria2k25",
+ // base: "/TechEuphoria2k25",
   base: "/",  // Adjust if neededz
   server: {
     host: "0.0.0.0",  // Allow external access
