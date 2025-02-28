b28f1a0e38342e24022211a92990fdaa8b0c7aa3
Vinay Siddha <121566763+VinaySiddha@users.noreply.github.com>
2025-02-28 18:37:37 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.JPG'],
-  // base: '/',
-  // server: {
-  //   host: "0.0.0.0",  // Allow external access
-  //   port: process.env.PORT || 10000,  // Use Render's assigned port
-  //   strictPort: true,
-  //   allowedHosts: ["techeuphoria2k25.onrender.com"],  // Allow Render domain
-  // },
+  base: '/',
+  server: {
+    host: "0.0.0.0",  // Allow external access
+    port: process.env.PORT || 10000,  // Use Render's assigned port
+    strictPort: true,
+    allowedHosts: ["techeuphoria2k25.onrender.com"],  // Allow Render domain
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
