f6547df147b7a81189c7964b36d4f519a72c3460
PrudhviSaranya <prudhvisaranya20@gmail.com>
2025-03-04 15:20:50 +0530

    Fix grammar and clarity in participation fee descriptions on RulesPage

--- src/pages/RulesPage.tsx
+++ src/pages/RulesPage.tsx
@@ -6,12 +6,12 @@ function RulesPage() {
 
   const rules = [
     "Participants must adhere to the event schedule.",
-    "A fee of Rs.200/- per head needs to be paid to participate in department and college level events.",
-    "A maximum of 04 events can be participated in with the payment made.",
+    "A fee of Rs.200/- per head need to be paid to participate in department and college level events.",
+    "A maximum of 04 events can be participated with the payment made.",
     "A maximum of 03 department level events and 01 college level event (or) 02 department level and 02 college level events (or) 01 department level and 03 college level events can be participated in.",
     "Any form of cheating will result in disqualification.",
     "Separate registrations are required to participate in cultural competitions.",
-    "A fee of Rs.100/- per head needs to be paid for cultural competitions.",
+    "A fee of Rs.100/- per head need to be paid for cultural competitions.",
     "For Instance a Total of 05 Members are participating in a group dance then , Rs.500/- ( 100/- per head) to be paid to perform",
     "Winners and runners-up awards are given for technical and cultural competitions.",
     "All participants should wear the identity “BAND” provided to you.",
